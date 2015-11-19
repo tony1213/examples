d3d7ce374de3baeb496db91a104641b1a2c0f8bc
Esteve Fernandez <esteve@osrfoundation.org>
2015-11-19 12:16:43 -0800

    Talker and listener examples in Python

---
+++ rclpy_examples/listener_py.py
@@ -0,0 +1,24 @@
+import sys
+
+import rclpy
+from rclpy.qos import qos_profile_default
+from std_msgs.msg import String
+
+
+def chatter_callback(msg):
+    print("I heard: [%s]" % msg.data)
+
+def main(args=None):
+    if args is None:
+        args = sys.argv
+
+    rclpy.init(args)
+
+    node = rclpy.create_node("listener")
+
+    sub = node.create_subscription(String, "chatter", chatter_callback, qos_profile_default)
+
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
---
+++ rclpy_examples/talker_py.py
@@ -0,0 +1,30 @@
+import sys
+from time import sleep
+
+import rclpy
+from rclpy.qos import qos_profile_default
+from std_msgs.msg import String
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv
+
+    rclpy.init(args)
+
+    node = rclpy.create_node("talker")
+
+    chatter_pub = node.create_publisher(String, "chatter", qos_profile_default)
+
+    msg = String()
+
+    i = 1
+    while True:
+      msg.data = "Hello World: {0}".format(i)
+      i += 1
+      print("Publishing: '{0}'".format(msg.data))
+      chatter_pub.publish(msg)
+      sleep(1)
+
+if __name__ == '__main__':
+    main()
